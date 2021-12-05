18c1bf3ee8e4144cd3107c85fac8b1b32a7acda3
Roberto <mario24roberto@gmail.com>
2021-12-04 23:12:02 -0600

    Agregando mas atributos a nuestro nuevo enlace

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -2,8 +2,10 @@ const enlace = document.createElement('A');
 enlace.textContent= 'Nuevo Enlace';
 enlace.href='./nuevo-enlace';
 enlace.target = '_blank';
-
+enlace.setAttribute('data-enlace', 'Nuevo Enlace')
 console.log(enlace);
+
+
 // Navegacion
 const navegacion = document.querySelector('.navegacion');
 console.log(navegacion.children);
