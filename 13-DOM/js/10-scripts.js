e0e10505a1d3137e3b5ed5bfc9743eeea4cce394
Roberto <mario24roberto@gmail.com>
2022-01-15 22:08:12 -0600

    Agregando un nuevo enlace

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -1,13 +1,15 @@
 const enlace = document.createElement('A');
-enlace.textContent= 'Nuevo Enlace';
-enlace.href='./nuevo-enlace';
-enlace.target = '_blank';
-enlace.setAttribute('data-enlace', 'Nuevo Enlace')
-enlace.classList.add('alguna-clase');
-console.log(enlace);
-
-
-// Navegacion
+enlace.textContent='Nuevo Enlace';
+enlace.href='./nuevo_enlace';
+enlace.target= '_blank'
+console.log(enlace)
+enlace.setAttribute('date-enlace', 'Nuevo Enlace');
+enlace.classList.add('Nueva-Clase')
 const navegacion = document.querySelector('.navegacion');
-console.log(navegacion.children);
-navegacion.insertBefore(enlace,navegacion.children[1]);
+// console.log(navegacion.children);
+enlace.onclick=mifuncion;
+navegacion.insertBefore(enlace, navegacion.children[1]);
+
+function mifuncion(){
+    alert('Diste Click')
+}
