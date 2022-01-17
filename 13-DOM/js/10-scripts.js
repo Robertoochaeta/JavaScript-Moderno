1cd96775a466e10ac47f5074e1a782b7b8169cd2
Roberto <mario24roberto@gmail.com>
2022-01-17 11:46:54 -0600

    Agregando un nuevo elemento html a card

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -15,5 +15,34 @@ function mifuncion(){
 }
 // Crear un card de forma dinamica
 const parrafo1= document.createElement('p');
+parrafo1.textContent= 'Concierto'
+
+parrafo1.classList.add('categoria', 'concierto')
+
 const parrafo2 = document.createElement('p');
+parrafo2.textContent='Concierto de rock'
+parrafo2.classList.add('titulo')
+
 const parrafo3 = document.createElement('p');
+parrafo3.textContent= '800 por persona';
+parrafo3.classList.add('precio')
+
+// Crear Div con la clase de info
+const info = document.createElement('div');
+info.classList.add('info');
+info.appendChild(parrafo1)
+info.appendChild(parrafo2)
+info.appendChild(parrafo3)
+
+const imagen = document.createElement('img');
+imagen.src = 'img/hacer2.jpg'
+
+const card = document.createElement('div');
+card.classList.add('card');
+// Asignar Imagen
+card.appendChild(imagen);
+card.appendChild(info);
+
+// Insertar en el html
+const contenedore = document.querySelector('.hacer .contenedor-cards');
+contenedore.insertBefore(card, contenedore.children[0])
